cbcfad1757548e7717827bff4965824854d1ef93
Laurence <sirlaurence@protonmail.com>
2022-07-01 21:11:33 -0700

    Movement input

--- main.cpp
+++ main.cpp
@@ -1,15 +1,45 @@
 #include "raylib.h" //-lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+
+
+void Input(int& x, int& y, int speed){
+    if(IsKeyDown(KEY_D)) x += speed;
+    if(IsKeyDown(KEY_A)) x -= speed;
+
+    // the inverse with this library
+    if(IsKeyDown(KEY_W)) y -= speed; 
+    if(IsKeyDown(KEY_S)) y += speed;
+}
+
 int main(){
 
-
+    //create window
     int width{350};
-   
+    int height{350};
+    InitWindow(width,height,"Hello!");
 
-    InitWindow(width,200,"Hello!");
+
+
+    int initX{width/2};
+    int initY{height/2};
+
+    int xCoord{initX};
+    int yCoord{initY};
+
+    int moveSpeed{10};
+
+
+    SetTargetFPS(60);
+    //main game loop
     while(!WindowShouldClose()){
                 
-        BeginDrawing();
-        ClearBackground(WHITE); //frame buffer 
+        BeginDrawing(); //creates an double buffer (raylib magic)
+        ClearBackground(WHITE); 
+
+
+        DrawCircle(xCoord,yCoord, 15.0f, BLUE);
+
+        Input(xCoord,yCoord, moveSpeed);
+
         EndDrawing();
     }
 
