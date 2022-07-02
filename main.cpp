d0b644be260cd48c86ce8e92f69bb1918d800b6b
Laurence <sirlaurence@protonmail.com>
2022-07-01 20:46:02 -0700

    Basic Game window

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
-#include <iostream>
-#include "raylib.h"
-
+#include "raylib.h" //-lraylib -lGL -lm -lpthread -ldl -lrt -lX11
 int main(){
 
 
     int width{350};
+   
 
-    InitWindow(width,1000,"Hello!");
+    InitWindow(width,200,"Hello!");
+    while(!WindowShouldClose()){
+                
+        BeginDrawing();
+        ClearBackground(WHITE); //frame buffer 
+        EndDrawing();
+    }
 
     return 0;
 }
